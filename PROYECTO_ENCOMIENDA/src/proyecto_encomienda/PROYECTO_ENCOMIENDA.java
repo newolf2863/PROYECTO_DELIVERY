4c5a1231b37f522c2865f1e2885361b4b867abc8
newolf2863 <issacdelacadena@gmail.com>
2024-07-11 09:18:08 -0500

    ModMain Main 1.0.0
    Agregacion de print

--- PROYECTO_ENCOMIENDA/src/proyecto_encomienda/PROYECTO_ENCOMIENDA.java
+++ PROYECTO_ENCOMIENDA/src/proyecto_encomienda/PROYECTO_ENCOMIENDA.java
@@ -14,6 +14,7 @@ public class PROYECTO_ENCOMIENDA {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Hola");
         // TODO code application logic here
     }
     
