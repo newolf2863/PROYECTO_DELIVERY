5fb6f2abd0f92469e60fd1ffddea1ebd6a000045
newolf2863 <issacdelacadena@gmail.com>
2024-07-13 16:27:19 -0500

    Mod_Incidentes Inventario()
    Clase para realizar una verificacion

--- PROYECTO_ENCOMIENDA/src/proyecto_encomienda/INCIDENTES/Inventario.java
+++ PROYECTO_ENCOMIENDA/src/proyecto_encomienda/INCIDENTES/Inventario.java
@@ -4,10 +4,50 @@
  */
 package proyecto_encomienda.INCIDENTES;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 /**
  *
  * @author Issac
  */
+
 public class Inventario {
-    
+    private List<Paquete> lista;
+    private Date fechaIngreso;
+
+    public Inventario() {
+        this.lista = new ArrayList<>();
+        this.fechaIngreso = new Date();
+    }
+
+    public void añadirPaquete(Paquete paquete) {
+        lista.add(paquete);
+        System.out.println("Paquete añadido al inventario.");
+    }
+
+    public void verificarEstado() {
+        System.out.println("Verificando el estado del inventario.");
+    }
+
+    public List<Paquete> getLista() {
+        return lista;
+    }
+
+    public void setLista(List<Paquete> lista) {
+        this.lista = lista;
+    }
+
+    public Date getFechaIngreso() {
+        return fechaIngreso;
+    }
+
+    public void setFechaIngreso(Date fechaIngreso) {
+        this.fechaIngreso = fechaIngreso;
+    }
+    public class Paquete {
+    // Atributos y métodos de la clase Paquete
+    }
 }
+
