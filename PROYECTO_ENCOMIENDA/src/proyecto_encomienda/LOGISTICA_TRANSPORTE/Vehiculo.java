cc8e80541b370f2daaa5d516600cc0cb5b781c97
juansuarezb <94476034+juansuarezb@users.noreply.github.com>
2024-07-13 19:53:39 -0500

    Update Vehiculo.java
    PruebaCommitGrupoLogisticaTransporte

--- PROYECTO_ENCOMIENDA/src/proyecto_encomienda/LOGISTICA_TRANSPORTE/Vehiculo.java
+++ PROYECTO_ENCOMIENDA/src/proyecto_encomienda/LOGISTICA_TRANSPORTE/Vehiculo.java
@@ -4,10 +4,20 @@
  */
 package proyecto_encomienda.LOGISTICA_TRANSPORTE;
 
-/**
- *
- * @author Issac
- */
-public class Vehiculo {
+
+    public class Vehiculo {
     
-}
+    public String numeroPlaca;
+    public double capacidad;
+    public String [] paquete;
+
+    public Vehiculo(String numeroPlaca, double capacidad) {
+        this.numeroPlaca = numeroPlaca;
+        this.capacidad = capacidad;
+    }
+    
+    
+    public boolean obtenerPaquete(){
+        return false;
+    }
+
