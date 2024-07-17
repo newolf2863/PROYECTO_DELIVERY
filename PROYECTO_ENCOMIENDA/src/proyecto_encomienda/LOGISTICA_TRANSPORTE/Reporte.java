f11a25ee06f1ba050faf38b8af4fd97f36d79034
edisonivaan1 <edison.jimenez03@epn.edu.ec>
2024-07-16 23:04:45 -0500

    Cambios
    Cambios en el módulo de Transporte

--- PROYECTO_ENCOMIENDA/src/proyecto_encomienda/LOGISTICA_TRANSPORTE/Reporte.java
+++ PROYECTO_ENCOMIENDA/src/proyecto_encomienda/LOGISTICA_TRANSPORTE/Reporte.java
@@ -1,21 +1,52 @@
 
 
-
+package proyecto_encomienda.LOGISTICA_TRANSPORTE;
+import java.util.Date;
+import java.util.List;
 /**
  *
- * @author juand_jus2zd
+ * @author ediso
  */
 public class Reporte {
-    public boolean estado;
-    public String descripcion;
-    
-    public int actualizarEstadoPaquete(){
-        return 0;
-    }
-    
-    public String generarReporte(){
-        return "Reporte{" + "estado=" + estado + ", descripcion=" + descripcion + '}';
+    private Envio envio;
+
+    // Constructor
+    public Reporte(Envio envio) {
+        this.envio = envio;
     }
 
-    
+    // Método para verificar el estado del paquete
+    public void verificarEstado() {
+        String estado = envio.getEstado();
+        System.out.println("El estado del envío es: " + estado);
+    }
+
+    // Método para generar un reporte
+
+    //ARREGLAR
+    public void generarReporte() {
+        if (!envio.getPaquetes().isEmpty()) {
+            System.out.println("Número de Tracking del paquete: " + envio.getPaquetes().get(0).obtenerCodigo());
+        }
+        //¿A QUE SE REFIERE LA DESCRIPCIÓN DEL PAQUETE?
+        System.out.println("Descripción del paquete: " + envio.getDescripcion());
+        System.out.println("Estado del envio: " + envio.getEstado());
+        
+        System.out.println("Fecha del reporte: " + new Date());
+    }
+
+    public static void generarYMostrarReportes(List<Envio> envios) {
+        int i = 1;
+        for (Envio envio : envios) {
+            Reporte reporte = new Reporte(envio);
+            System.out.println("*****************************************************************************************");
+            System.out.println("***********************REPORTE DEL ENVIO " + i + " ***********************");
+            reporte.generarReporte();
+            System.out.println("*****************************************************************************************");
+            System.out.println("***********************INFORMACION DEL ENVIO " + i + "***********************");
+            envio.imprimirEnvio();
+            i++;
+        }
+    }
+
 }
