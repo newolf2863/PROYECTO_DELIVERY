ffefc6eb2ef99824a9b353a9f58ef635153dbbb8
Josue Peralta <peraltajosue115@gmail.com>
2024-08-04 17:07:47 -0500

    cambios
    comentarios

--- PROYECTO_ENCOMIENDA/src/mod_incidentes/Incidente.java
+++ PROYECTO_ENCOMIENDA/src/mod_incidentes/Incidente.java
@@ -5,10 +5,27 @@
 package mod_incidentes;
 
 /**
- *
- * @author Rodrigo Haro
+ * Clase abstracta que representa un incidente.
+ * Define los métodos que deben ser implementados por las clases que hereden de esta.
+ * 
+ * @autor Rodrigo Haro
  */
 public abstract class Incidente {
+    
+    /**
+     * Método abstracto para registrar un incidente.
+     * Debe ser implementado por las clases que hereden de Incidente.
+     * 
+     * @return Una cadena de texto con la descripción del registro del incidente.
+     */
     public abstract String registrar();
+    
+    /**
+     * Método abstracto para resolver un incidente.
+     * Debe ser implementado por las clases que hereden de Incidente.
+     * 
+     * @param argumentos Un array de Strings que contiene los argumentos necesarios para resolver el incidente.
+     * @return Una cadena de texto con la descripción de la resolución del incidente.
+     */
     public abstract String resolver(String[] argumentos);
 }
